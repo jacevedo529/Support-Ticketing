c1540a2867f74b4e0c18a5ae41075620dc21b703
Jose Acevedo <jacevedo@infragistics.com>
2021-12-30 22:21:54 -0500

    Add Generate JWT Token

---
+++ UsersService/Utilities/JwtTokenUtility.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using Repository.Models.Identity;
+using Services.Interfaces;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Services.Utilities
+{
+    public class JwtTokenUtility : IJwtTokenUtility
+    {
+        public string GenerateJwtToken(ApplicationUser user)
+        {
+            // generate token that is valid for 7 days
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(Constants.TOKEN_SECRET);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] { new Claim(ClaimTypes.Sid, user.Id.ToString()) }),
+                Expires = DateTime.UtcNow.AddMinutes(30),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
